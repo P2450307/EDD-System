89795cd20f13deb17bdc561bb8dbed8fb7dee8ce
Shahnawaz Yusuf <shahnawazyusuf786@gmail.com>
2020-02-11 14:26:50 +0000

    added more code to tst.staff

--- EDDTesting/tstStaff.cs
+++ EDDTesting/tstStaff.cs
@@ -16,5 +16,46 @@ namespace EDDTesting
             Assert.IsNotNull(Staff);
 
         }
+
+        [TestMethod]
+        public void StaffIDPropertyOK()
+        {
+            clsStaff Staff = new clsStaff();
+
+           //create test data to assign to property
+            Int32 TestData = 1;
+
+            //assign data to the property
+            Staff.StaffID = TestData;
+
+            //test to see if both alues are equal
+            Assert.AreEqual(Staff.StaffID, TestData);
+        }
+
+        [TestMethod]
+        public void FullNamePropertyOK()
+        {
+            clsStaff Staff = new clsStaff();
+
+            string TestData = "John Smith";
+
+            Staff.FullName = TestData;
+
+            Assert.AreEqual(Staff.FullName, TestData);
+            
+        }
+
+        [TestMethod]
+        public void DateOfBirthPropertyOK()
+        {
+            clasStaff Staff = new clasStaff();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            Staff.DateOfBirth = TestData;
+
+            Assert.AreEqual(Staff.DateOfBirth, TestData);
+        }
+        
     }
 }
