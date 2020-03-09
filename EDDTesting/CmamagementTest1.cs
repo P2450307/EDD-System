81a3b2e5db9dcab93cc1137fe6750abff429ebb0
Guy Amatcha <P2403300@my365.dmu.ac.uk>
2020-03-09 12:07:03 +0000

    first test case made

---
+++ EDDTesting/CmamagementTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EDDClasses;
+
+
+namespace EDDTesting
+{
+    [TestClass]
+    public class CmamagementTest1
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create instance of class
+            clsCustomer Customer = new clsCustomer();
+            //test to see it exists
+            Assert.IsNotNull(Customer);
+
+        }
+    }
+}
