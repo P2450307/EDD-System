b8618ce1b567fbe9c4a036d779e896ed44bc9bb4
Elliot Forde <P2416030@my365.dmu.ac.uk>
2020-06-01 00:19:06 +0100

    Added Customer.aspx

---
+++ EDDFrontOffice/Customer.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EDDClasses;
+
+public partial class Customer: System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void BtnSubmit_Click(object sender, EventArgs e)
+    {
+        ClsCustomer stock = new ClsCustomer();
+        string ProductDetails = txtProductDetails.Text;
+        string Price = txtPrice.Text;
+        string DateAcquired = txtDateAcquired.Text;
+        string InStock = txtInStock.Text;
+        string Error = "";
+        Error = stock.Valid(ProductDetails, DateAcquired);
+        if (Error == "")
+        {
+            stock.ProductDetails = ProductDetails;
+            stock.Price = Convert.ToDecimal(txtPrice.Text);
+            stock.DateAcquired = Convert.ToDateTime(txtDateAcquired.Text);
+            stock.Result = Convert.ToBoolean(txtInStock.Text);
+            Session["Stock"] = stock;
+            Response.Write("StockViewer.aspx");
+        }
+        else
+        {
+            lblError.Text = Error;
+        }
+    }
+
+    protected void BtnCancel_Click(object sender, EventArgs e)
+    {
+
+    }
+}
